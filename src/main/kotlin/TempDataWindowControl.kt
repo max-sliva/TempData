b25d4f52cbba6f9bee6cecd2c9487c39006ba393
user <persimon@inbox.ru>
2022-12-30 01:39:50 +0500

    started csv work

--- src/main/kotlin/TempDataWindowControl.kt
+++ src/main/kotlin/TempDataWindowControl.kt
@@ -1,2 +1,25 @@
+import javafx.event.ActionEvent
+import javafx.fxml.FXML
+import javafx.scene.layout.BorderPane
+import javafx.stage.FileChooser
+import java.io.File
+import java.nio.file.Paths
+
 class TempDataWindowControl {
+    @FXML
+    lateinit var mainPane: BorderPane
+
+    fun openCSVfile(actionEvent: ActionEvent) {
+        println("open file")
+        val fileChooser = FileChooser().apply{
+            title = "Open Excel File"
+            val currentPath: String = Paths.get(".").toAbsolutePath().normalize().toString()
+            initialDirectory = File(currentPath)
+            extensionFilters.addAll(
+                FileChooser.ExtensionFilter("CSV Files", "*.csv"),
+                FileChooser.ExtensionFilter("All Files", "*.*")
+            )
+        }
+        val file = fileChooser.showOpenDialog(mainPane.scene.window)
+    }
 }
